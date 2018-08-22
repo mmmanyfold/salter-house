90a0781d8a86205312634de5acc706aaab6cc054
David A. Viramontes <dviramontes@gmail.com>
2018-08-21 21:47:11 -0600

    remove homePageEventRegister flag

--- assets/js/theme/page-manager.js
+++ assets/js/theme/page-manager.js
@@ -2,36 +2,31 @@ import $ from 'jquery';
 import Url from 'url';
 import urlUtils from "./common/url-utils";
 
-let homePageEventRegister = false;
-
 export default class PageManager {
 
     constructor(context) {
         this.currentUrl = urlUtils.getUrl();
         this.context = context;
-        homePageEventRegister = !homePageEventRegister;
         console.log(this.currentUrl);
-        if (homePageEventRegister) {
-            if (this.currentUrl !== '/') {
-                const selector = $('#newArrivals-link');
-                selector
-                    .siblings('div.category-dot-selected-orange')
-                    .toggleClass('u-hiddenVisually');
-                selector
-                    .siblings('div.category-dot-unselected-orange')
-                    .toggleClass('u-hiddenVisually');
-            } else if (this.currentUrl === '/') {
-                $('.navPages-action')
-                    // filter out new category link
-                    .filter('a[id!="newArrivals-link"]')
-                    .siblings('div.u-hiddenVisually.category-dot-selected-green')
-                    .toggleClass('u-hiddenVisually');
-            }
-            $('#newArrivals-link').click(() => {
-                const url = Url.parse(window.location.href, true);
-                window.location.href = `${url.protocol}//${url.host}/#new`
-            });
+        if (this.currentUrl !== '/') {
+            const selector = $('#newArrivals-link');
+            selector
+                .siblings('div.category-dot-selected-orange')
+                .toggleClass('u-hiddenVisually');
+            selector
+                .siblings('div.category-dot-unselected-orange')
+                .toggleClass('u-hiddenVisually');
+        } else if (this.currentUrl === '/') {
+            $('.navPages-action')
+            // filter out new category link
+                .filter('a[id!="newArrivals-link"]')
+                .siblings('div.u-hiddenVisually.category-dot-selected-green')
+                .toggleClass('u-hiddenVisually');
         }
+        $('#newArrivals-link').click(() => {
+            const url = Url.parse(window.location.href, true);
+            window.location.href = `${url.protocol}//${url.host}/#new`
+        });
     }
 
     type() {
