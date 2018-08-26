411628edef4687d6480349d9cd58435da676e0c3
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-08-26 15:03:31 -0600

    Takes function for scrolling out of export

--- assets/js/theme/global/header-hide.js
+++ assets/js/theme/global/header-hide.js
@@ -1,11 +1,47 @@
 import $ from 'jquery';
 import urlUtils from "../common/url-utils";
 
-export default function() {
-    const header = $('header');
+// Hide Header on on scroll down
+let didScroll;
+let lastScrollTop = 0;
+
+let isHome;
+
+function hasScrolled() {
     const delta = 5;
 
-    let isHome = "/" == urlUtils.getUrl();
+    let headerbarHeight = header.outerHeight();
+    let st = window.scrollY;
+
+    // Make sure they scroll more than delta
+    if (Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    if (isHome && st >= 584) {
+        header.removeClass('header-home');
+    } else {
+        header.addClass('header-home');
+    }
+    // If they scrolled down and are past the headerbar, add class .header-up.
+    // This is necessary so you never see what is "behind" the headerbar.
+    if (st > lastScrollTop && st > headerbarHeight) {
+        // Scroll Down
+        header.removeClass('header-down').addClass('header-up');
+    } else {
+        // Scroll Up
+        if (st + $(window).height() < $(document).height()) {
+            header.removeClass('header-up').addClass('header-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
+
+export default function() {
+    const header = $('header');
+
+    isHome = "/" == urlUtils.getUrl();
+
 
     if (isHome) {
         header.addClass('header-home');
@@ -26,35 +62,4 @@ export default function() {
         }
 
     }, 250);
-// Hide Header on on scroll down
-    let didScroll;
-    let lastScrollTop = 0;
-
-    function hasScrolled() {
-        let headerbarHeight = header.outerHeight();
-        let st = window.scrollY;
-
-        // Make sure they scroll more than delta
-        if (Math.abs(lastScrollTop - st) <= delta)
-            return;
-
-        if (isHome && st >= 584) {
-            header.removeClass('header-home');
-        } else {
-            header.addClass('header-home');
-        }
-        // If they scrolled down and are past the headerbar, add class .header-up.
-        // This is necessary so you never see what is "behind" the headerbar.
-        if (st > lastScrollTop && st > headerbarHeight) {
-            // Scroll Down
-            header.removeClass('header-down').addClass('header-up');
-        } else {
-            // Scroll Up
-            if (st + $(window).height() < $(document).height()) {
-                header.removeClass('header-up').addClass('header-down');
-            }
-        }
-
-        lastScrollTop = st;
-    }
 }
