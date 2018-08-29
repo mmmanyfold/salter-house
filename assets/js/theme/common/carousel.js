e0dbff93c49ace9fe80ff20aeec79959f8509cf3
David A. Viramontes <dviramontes@gmail.com>
2018-08-28 19:02:58 -0600

    fix: should be vertical at all times

--- assets/js/theme/common/carousel.js
+++ assets/js/theme/common/carousel.js
@@ -3,34 +3,9 @@ import 'slick-carousel';
 
 export default function () {
     const $carousel = $('[data-slick]');
-    const autoplaySpeed = +$('[data-carousel-swap-frequency]');
 
     if ($carousel.length) {
-        $carousel.slick({
-            dots: true,
-            mobileFirst: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            arrows: false,
-            vertical: false,
-            verticalSwiping: false,
-            autoplaySpeed,
-            lazyLoad: "anticipated",
-            responsive: [{
-                breakpoint: 301,
-                settings: {
-                    vertical: true,
-                    verticalSwiping: false,
-                },
-            }, {
-                breakpoint: 801,
-                settings: {
-                    vertical: false,
-                    verticalSwiping: false,
-                },
-            }]
-        });
+        $carousel.slick();
     }
 
     // Alternative image styling for IE, which doesn't support objectfit
