d3c5ad6f558d5eed4478ee1d9c804f76bb643b34
Michelle L <oreneemshi@gmail.com>
2018-09-02 12:28:40 -0600

    only show featured products on homepage, rename productGrid => productGridNew in home.js, … (#82)
    * only show featured products on homepage, rename productGrid => productGridNew in home.js, hide first category (new), append category name to .productGrid class
    
    * feat: load newest products onto /new category page
    
    * chore: format
    
    * remove: unused
    
    * fix: filter 'New' from category nav

--- assets/js/theme/home.js
+++ assets/js/theme/home.js
@@ -2,70 +2,17 @@ import PageManager from './page-manager';
 import urlUtils from "./common/url-utils";
 import navUtils from "./common/nav-utils";
 import $ from 'jquery';
-import api from "@bigcommerce/stencil-utils/src/api";
 
 export default class Home extends PageManager {
   constructor(context) {
     super(context);
     this.currentUrl = urlUtils.getUrl();
-    this.categoryProductsPerPage = 36;
   }
 
   async onReady() {
-    const $productGrid = $('.productGrid');
-    const nextPage = await this.getNextPage('/categories', {
-      page: 1,
-      limit: 12,
-      sort: 'newest',
-    });
-    const liNodes = this.processRawHtml(nextPage);
-    $productGrid.append(liNodes);
-    $productGrid.find('.new-label')
-      .each((i, p) => $(p).removeClass('u-hiddenVisually'));
-    this.bindEvents();
     navUtils(this.currentUrl);
     if ($(window).scrollTop() > 0) {
       $('.header').removeClass('header-home')
     }
   }
-
-  bindEvents() {
-    const $seeMoreBtn = $('#_see-more-btn');
-    const $productGrid = $('.productGrid');
-    const limit = this.categoryProductsPerPage;
-    let page = 2; // assumes page one has been loaded by onReady() call
-    $seeMoreBtn.click(async () => {
-      const nextPage = await this.getNextPage('/categories', {
-        page,
-        limit,
-        sort: 'newest',
-      });
-      const liNodes = this.processRawHtml(nextPage);
-      if (liNodes.length < this.categoryProductsPerPage) {
-        $('#_see-more-btn').hide();
-      }
-      $productGrid.append(liNodes);
-      page += 1;
-    });
-  }
-
-  async getNextPage(url, params) {
-    return new Promise((resolve, reject) =>
-      api.getPage(url, {
-        params: params
-      }, (err, res) => {
-        if (err) {
-          throw new Error(err);
-          return reject(err);
-        }
-        return resolve(res);
-      }));
-  }
-
-  processRawHtml(html) {
-    const template = document.createElement('div');
-    template.innerHTML = html;
-    return template.querySelectorAll('.productGrid li');
-  }
-
 }
